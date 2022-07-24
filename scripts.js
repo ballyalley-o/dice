313daa49bc6c1f07b535db739c1da10f528eb79b
bally <108769285+ballyalley-o@users.noreply.github.com>
2022-07-25 00:43:20 +1200

    working dice

--- scripts.js
+++ scripts.js
@@ -1,22 +1,35 @@
-/*var dice = {
-    sides: 6,
-    roll: function () {
-      var randomNumber = Math.floor(Math.random() * this.sides) + 1;
-      return randomNumber;
+
+  
+  let images = [
+    "dice-svg/dice-one.svg",
+    "dice-svg/dice-two.svg",
+    "dice-svg/dice-three.svg",
+    "dice-svg/dice-four.svg",
+    "dice-svg/dice-five.svg",
+    "dice-svg/dice-six.svg"];
+    let dice = document.querySelectorAll("img");
+
+
+   function roll(){
+        dice.forEach(function(die){
+            die.classList.add("shake");
+        });
+        setTimeout(function(){
+            dice.forEach(function(die){
+                die.classList.remove("shake");
+            });
+            let dieOneValue = Math.floor(Math.random()*6)
+            ;
+            let dieTwoValue = Math.floor(Math.random()*6)
+            ;
+            console.log(dieOneValue,dieTwoValue);
+            document.querySelector("#die-1").setAttribute("src", images[dieOneValue]);
+            document.querySelector("#die-2").setAttribute("src", images[dieTwoValue]);
+            document.querySelector("#total").innerHTML = "Dude, your ROLL is " + 
+            ( (dieOneValue +1) + (dieTwoValue +1) );
+        },
+        1000
+        );
+    
     }
-  }
-  
-  function printNumber(number) {
-    var placeholder = document.getElementById('placeholder');
-    placeholder.innerHTML = number;
-  }
-  
-  var button = document.getElementById('button');
-  
-  button.onclick = function() {
-    var result = dice.roll();
-    printNumber(result);
-  };
-  */
-  
-  
+
